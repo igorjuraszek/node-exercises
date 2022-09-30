c800ccf5337fb514eab6d3ece3d5404097271ea0
igorjuraszek <igorjuraszek@gmail.com>
2022-09-30 16:21:07 +0200

    adding cars

---
+++ basics.js
@@ -0,0 +1,78 @@
+import { format, startOfMonth, setMonth, getDaysInMonth } from "date-fns";
+
+// Array of 12
+
+let array_1 = Array.from(Array(12).keys());
+let array_2 = [...Array(12).keys()];
+let array_3 = Array.from({ length: 12 }, (_, i) => i);
+
+let cars = [
+  {
+    brand: "BMW",
+    plates: "SZY 623523",
+    year: 2007,
+  },
+  {
+    brand: "TOYOTA",
+    plates: "SZY 12345",
+    year: 2009,
+  },
+  {
+    brand: "TOYOTA",
+    plates: "SZY 00001",
+    year: 2008,
+  },
+  {
+    brand: "MERCEDES",
+    plates: "SZY 35123",
+    year: 2010,
+  },
+  {
+    brand: "BMW",
+    plates: "SZY 74234",
+    year: 2011,
+  },
+  {
+    brand: "SKODA",
+    plates: "SZY 00928",
+    year: 2012,
+  },
+  {
+    brand: "TOYOTA",
+    plates: "SZY 10000",
+    year: 2013,
+  },
+];
+
+// all plates
+cars.map((car) => {
+  return car.plates;
+});
+
+// brand list
+[
+  ...new Set(
+    cars.map((car) => {
+      return car.brand;
+    })
+  ),
+];
+
+// cars between 2010 and 2012 includes
+cars.filter((car) => {
+  return car.year >= 2010 && car.year <= 2012;
+});
+
+// date-fns
+
+// current date
+let currentDate = new Date();
+
+// days in month
+let daysInMonth = getDaysInMonth(currentDate);
+
+// name of month
+let nameOfMonth = format(currentDate, "MMMM");
+
+// array of month names
+Array.from({ length: 12 }, (_, index) => format(new Date(2022, index), "MMMM"));
---
+++ cars.js
@@ -0,0 +1,36 @@
+import _ from "lodash";
+import {} from "date-fns";
+import data from "./cars-database.json" assert { type: "json" };
+
+const cars = data.cars.map((car) => {
+  car.bookedRents = data.bookings.filter((rent) => {
+    if (rent.car !== car.id) return false;
+    rent.daysOfRent = data.bookedDays.filter((day) => {
+      return day.booking === rent.id;
+    });
+    return rent;
+  });
+  return car;
+});
+
+// get all cars count
+function getCarsCount() {
+  return;
+}
+
+// get brands
+function getBrands() {
+  return;
+}
+
+// reservations by month
+function getReservationsByMonth() {
+  return;
+}
+
+// reservations count by month
+function getReservationsCountByMonth() {
+  return;
+}
+
+// bookings for month
